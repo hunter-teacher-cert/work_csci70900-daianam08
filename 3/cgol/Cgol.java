1e4ed0838f7183cdba1eeea90787643a01be9378
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-07-01 18:59:29 +0000

    working on cgol

--- 3/cgol/Cgol.java
+++ 3/cgol/Cgol.java
@@ -5,11 +5,14 @@ import java.util.*;
    The Rules of Life:
    Survivals:
    * A cell with 2 or 3 living neighbours will survive for the next generation.
+   
    Death:
    * Each cell with >3 neighbours will die from overpopulation.
    * Every cell with <2 neighbours will die from isolation.
+   
    Birth:
    * Each dead cell adjacent to exactly 3 living neighbours is a birth cell. It will come alive next generation.
+   
    NOTA BENE:  All births and deaths occur simultaneously. Together, they constitute a single generation
 */
 
@@ -19,13 +22,29 @@ public class Cgol
   //initialize empty board (all cells dead)
   public static char[][] createNewBoard(int rows, int cols) {
 
+   
+  char [][] newBoard=new char[rows][cols];
+
+
+return newBoard;
   }
 
 
   //print the board to the terminal
   public static void printBoard(char[][] board) {
+  
+  
 
-  }
+  for (int i=0;i<board.length;i++)
+  {
+    for (int j=0;j<board[0].length;j++)
+    {
+      System.out.print(board[i][j]);
+
+    } //for int j
+System.out.println();
+  };//for int i
+  }//pri9ntBoard
 
 
   //set cell (r,c) to val
@@ -36,7 +55,7 @@ public class Cgol
 
   //return number of living neigbours of board[r][c]
   public static int countNeighbours(char[][] board, int r, int c) {
-
+return 0;
   }
 
 
@@ -46,22 +65,22 @@ public class Cgol
      (alive 'X', dead ' ')
   */
   public static char getNextGenCell(char[][] board,int r, int c) {
-
+    
+return 't';
   }
 
 
   //generate new board representing next generation
   public static char[][] generateNextBoard(char[][] board) {
-
+return board;
   }
 
 
-  public static void main( String[] args )
-  {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public static void main( String[] args ){
+
     char[][] board;
     board = createNewBoard(25,25);
-    //breathe life into some cells:
+     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//breathe life into some cells:
     setCell(board, 0, 0, 'X');
     setCell(board, 0, 1, 'X');
     setCell(board, 1, 0, 'X');
