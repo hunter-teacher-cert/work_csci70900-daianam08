bb595c0d7535eb070832efce6a3b404e7effc6bd
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-07-03 01:31:32 +0000

    working on first player move

--- 2/Mancala.java
+++ 2/Mancala.java
@@ -1,4 +1,6 @@
-//redid
+//MANCALA 2.0
+//redid on my own
+
 import java.io.*;
 import java.util.*;
 
@@ -7,19 +9,40 @@ public class Mancala{
   public static void main(String[]args){
     int aiBoard[]={0,4,4,4,4,4,4};
     int playerBoard[]={4,4,4,4,4,4,0};
-    
+
     boolean gameOn=false;
-    if (gameOn=false){
-      aiBoard[]={0,4,4,4,4,4,4};
-      playerBoard[]={4,4,4,4,4,4,0};
-      System.out.println("Welcome to Mancala! Choose a position on your side to start the game!");
+    if (gameOn==false){
+      aiBoard[0]=0;
+      playerBoard[6]=0;
+      for (int i=1; i<aiBoard.length; i++){
+        aiBoard[i]=4;
+      }
+      for (int i=0; i<playerBoard.length-1; i++){
+        playerBoard[i]=4;
+      }
+      System.out.println("Welcome to Mancala! Choose a position above your side to start the game!");
       board(aiBoard,playerBoard);
+
+      Scanner num = new Scanner(System.in);
+      int play=num.nextInt();
+      if (play>1 || play<6){
+      playerFirstPlay(gameOn,playerBoard, play);
+      } else {
+        System.out.println("Sorry, choose a number between 1 and 6");
+        play=num.nextInt();
+      }
     }//end gameOnFalse
 
-    if (gameOn==true;){
-      
-
-
+    if (gameOn==true){
+      int turn=0;
+      board(aiBoard,playerBoard);
+      if (turn % 2==0){
+        System.out.print("AI turn");
+        //playerTurn();
+      } else {
+        System.out.print("Player turn");
+        //aiTurn();
+      }
 
 
     }//end gameOnTrue
@@ -27,6 +50,25 @@ public class Mancala{
 
   }//end main
 
+  public static void playerFirstPlay(boolean on, int playerMove[], int number/*playerboard imported*/){
+    on=true;
+    int hand=0;
+    for (int i=0; i<playerMove.length-1; i++){
+      if (number==i){
+        hand=playerMove[i]-7;
+        playerMove[i]=0;
+      }
+    }
+    for (int x=1; x<hand; x++){
+      playerMove[x]=playerMove[x]-1;
+    }
+    if (hand==0){
+
+    }
+
+
+
+  }//end first move
 
 
   public static void board(int [] ai, int [] player){
@@ -35,12 +77,12 @@ public class Mancala{
       System.out.print(ai[i]+" ");
     }
     System.out.print("\n");
-    for (int i=1; i<player.length; i++){
+    for (int i=0; i<player.length-1; i++){
         System.out.print(player[i]+" ");
      }
     System.out.print("\n");
 
-    System.out.println("1 2 3 4 5 6"+"\n" + "player position");
+    System.out.println("6 5 4 3 2 1"+"\n" + "player position");
     System.out.println("Mancala: ai="+ai[0]+", player="+player[6]);
 // display(example) 
 //ai poition
