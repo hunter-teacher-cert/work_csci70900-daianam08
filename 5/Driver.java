253d643dba7959b419fa14a983c2b55c8a952d79
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-07-07 19:00:59 +0000

    need to check time and driver, almot done super array

--- 5/Driver.java
+++ 5/Driver.java
@@ -14,6 +14,23 @@ public class Driver {
 
     //above for printing or you can use below as well:
     String s= time.toString();
+
+
+    Time time1 = new Time(9, 30, 0.0);
+    Time time2 = time1;
+    Time time3 = new Time(9, 30, 0.0);
+
+    time1.equals(time3);
+    
   }//end main()
+   
+ //"that" refers to time3 while "this" refers to time 1. "this" is a keyword, "that is a variable name"
+  public boolean equals(Time that) {
+        return this.hour == that.hour
+            && this.minute == that.minute
+            && this.second == that.second;
+  }
+
+// That’s all there is to it. Static methods and instance methods do the same thing, and you can convert from one to the other with just a few changes.
 
 }//end class
--- 5/Time.java
+++ 5/Time.java
@@ -75,4 +75,24 @@ public void setSecond(int second){
   this.second=second;
 }
 
+// This implementation of add does not modify either of the parameters. Instead, it creates and returns a new Time object.
+public Time add(Time t2) {
+    Time sum = new Time();
+    sum.hour = this.hour + t2.hour;
+    sum.minute = this.minute + t2.minute;
+    sum.second = this.second + t2.second;
+
+    if (sum.second >= 60.0) {
+        sum.second -= 60.0;
+        sum.minute += 1;
+    }
+    if (sum.minute >= 60) {
+        sum.minute -= 60;
+        sum.hour += 1;
+    }
+    return sum;
+}
+
+
+
 }//end class timeclient
