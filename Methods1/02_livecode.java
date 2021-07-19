25adf994619f3679089f294a83413f8cfdec4bc4
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-07-19 01:48:27 +0000

    changes and additions

--- Methods1/02_livecode.java
+++ Methods1/02_livecode.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class MethodsDEMO{
   public static void main(String[] args){
+    System.out.println(isDivisible()); //use this to test our code! comment out when working on the stuff below.
+
     //after setting up isDivisible, do the following on your own:
     //What happens when isDivisible is false? How should you set up your conditional?
     if(isDivisible()== true) {
@@ -16,22 +18,26 @@ public class MethodsDEMO{
 
 //we will be using user input to practice using Scanner.
 //review with students how to find out if something is divisible by another number(gives remainder of 0 when divided. reminder of the use of "/" vs "%" !!!)
-  public static boolean isDivisible () {
-    System.out.print("Is this divisible? Enter one number: ");//this line will be given
+ public static boolean isDivisible () {
+    System.out.print("Is this divisible? Enter a big number: ");//this line will be given
 
     //who can walk me through Scanner?
     Scanner in= new Scanner(System.in);
     int n = in.nextInt();
     //remember to store in(your input) into a new variable to be used later
 
-    System.out.print("Enter another number: ");//this line will be given
+    System.out.print("Enter smaller number: ");//this line will be given
 
-    //do we need to redo Scanner? how do we store our next value?
+    //PAUSE
+    //do we need to redo Scanner? what does in.nextInt() do?
+    //how do we store our next value?
     int m = in.nextInt();
 
-    //Let's put this all together. How do we use the inputs to figure out if the numbers are divisible? When should we return true/false?
+    //PAUSE. Let's put this all together:
+    //How do we use the inputs to figure out if the numbers are divisible?
+    //When should we return true/false?
     //***reminder of the use of "/" vs "%" !!!*** SUPER IMPORTANT
-    if (n%m == 0){
+    if (n%m == 0){//use divide on purpose, maybe even use println to see what happens and why it is wrong
     return true;
     } else {
       return false;
