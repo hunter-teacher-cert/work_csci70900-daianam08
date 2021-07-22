fa1033af155d09d4f1e98ffbea37dfa814f7e844
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-07-22 17:48:17 +0000

    updated and added some stuff

--- DS/sort1/SortDemoDriver.java
+++ DS/sort1/SortDemoDriver.java
@@ -5,33 +5,33 @@ public class SortDemoDriver {
     public static void main(String[] args) {
 
 	SortDemo sd = new SortDemo();
+	/*
 	System.out.println(sd);
 
-	SortDemo sd2 = new SortDemo(20);
+	  SortDemo sd2 = new SortDemo(20);
 	System.out.println(sd2);
-
-
+		
+	// part 2 test lines 
 	int i;
 	i = sd.findSmallestIndex(0);
-	System.out.println("sd[" + i + "] = "+sd.get(i)+" : " + sd);
+	System.out.println("sd["+ i +"] = "+sd.get(i)+" : " + sd);
+
 	i = sd2.findSmallestIndex(0);
-	System.out.println("sd2[" + i + "] = "+sd2.get(i)+" : " + sd2);
-
-  /* part 2 test lines */
-  
-
-	/* part 3 test lines */
+	System.out.println("sd2["+ i + "] = "+sd2.get(i)+" : " + sd2);
 	
-	  System.out.println(sd);
+	// part 3 test lines 
+
+	System.out.println(sd);
 	  sd.sort();
 	  System.out.println(sd);
-	
-    Scanner myObj= new Scanner(System.in);
-    System.out.println("Enter a number to find on the array list");
-    int enterNumb= myObj.nextInt();
-    System.out.println(sd.linearSearch(enterNumb));
-  
-    }
+	*/
 
-    
+	System.out.println(sd);
+	sd.msortTest();
+	System.out.println(sd);
+	
+
+	      
+
+    }
 }
